a2406f5825d2d0328b5b21457ef2fa14d95097cd
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-23 10:17:04 +0100

    Add detailed file header to ev_abs.pl
    Include author information, creation date, and purpose of the file.
    Also, clarify copyright permissions and disclaimers.

--- ev_abs.pl
+++ ev_abs.pl
@@ -1,3 +1,31 @@
+/*  File:    ev_abs.pl
+    Author:  Roy Ratcliffe
+    Created: Jun 23 2026
+    Purpose: EV_ABS event consumer for Prolog
+
+Copyright (c) 2026, Roy Ratcliffe, Northumberland, United Kingdom
+
+Permission is hereby granted, free of charge,  to any person obtaining a
+copy  of  this  software  and    associated   documentation  files  (the
+"Software"), to deal in  the   Software  without  restriction, including
+without limitation the rights to  use,   copy,  modify,  merge, publish,
+distribute, sub-license, and/or sell copies  of   the  Software,  and to
+permit persons to whom the Software is   furnished  to do so, subject to
+the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 :- module(ev_abs, []).
 :- autoload(library(broadcast), [listen/2, unlisten/1]).
 :- autoload(library(redis), [redis_server/3, redis/3, redis/2]).
